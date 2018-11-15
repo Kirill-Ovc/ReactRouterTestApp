bdd45923c3c77768dad01bf7b4406721a8dbdef9
Kirill Ovcharenko <kirill.056@gmail.com>
2018-11-15 18:51:59 +0300

    Code Refactoring
    К предыдущему комиту

--- src/App.js
+++ src/App.js
@@ -5,51 +5,30 @@ import NotFound from './components/NotFound'
 import { routes } from './routes'
 import NavigationBar from './components/NavigationBar'
 
-const App = () => (
-  <Router>
-    <div>
-      <Navbar color="light" light expand="md">
-        <Nav navbar>
-          <NavItem>
-            <NavLink tag={Link} to="/">На главную</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/news">Новости</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/books">Книги</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/movies">Фильмы</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/about">О приложении</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-          </NavItem>
-        </Nav>
-        <Nav navbar className="ml-auto">
-          <NavItem >
-            <NavLink tag={Link} to="/login" >Войти</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink tag={Link} to="/profile">Профиль</NavLink>
-          </NavItem>
-        </Nav>
-      </Navbar>
-
-      <hr />
-      <div className="centerBlock">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/news" component={NewsContainer} />
-          <Route path="/books" component={BookList} />
-          <PrivateRoute path="/profile" component={ProfileContainer} />
-          <Route path="/login" component={LoginContainer} />
-          <Route path="/about" render={About} />
-          <Route component={NotFound} />
-        </Switch>
+const App = () => {
+  const renderSwitch = () => (
+    <Switch>
+      {routes.map(route => {
+        const RouteType = route.isPrivate ? PrivateRoute : Route
+        return (
+          <RouteType
+            key={route.path}
+            exact={route.isExact}
+            path={route.path}
+            component={route.component || NotFound}
+          />
+        )
+      })}
+      <Route component={NotFound} />
+    </Switch>
+  );
+  return (
+    <Router>
+      <div>
+        <NavigationBar routes={routes} />
+        <hr />
+        <div className="centerBlock">
+          {renderSwitch()}
         </div>
       </div>
     </Router>
